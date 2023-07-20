431bdc279a525aced32baa3c3e077da7a0026001
koreaNerd <pksksk0147@gmail.com>
2023-07-21 05:23:40 +0900

    style: git conflict 수정 커밋

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,15 @@ import App from "./App";
 import "./main.css";
 
 const router = createBrowserRouter([
-	{
-		path: "*",
-		element: <App />,
-	},
+  {
+    path: "*",
+    element: <App />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
