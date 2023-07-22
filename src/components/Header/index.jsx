31e0123acd6c4c04f476d57e41da4a977033f5cc
koreaNerd <pksksk0147@gmail.com>
2023-07-23 01:42:51 +0900

    chore: 로고 마진값 조정 커밋

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="absolute top-0 left-0">
       <div className="flex">
-        <h1 className="m-10 text-2xl font-bold">
+        <h1 className="mx-6 my-4 text-2xl font-bold">
           <a href="/">DBX</a>
         </h1>
       </div>
