6221d4f94b41cb57dc52cd681e46fcbfe8d7e10d
koreaNerd <pksksk0147@gmail.com>
2023-07-24 20:06:03 +0900

    멘도링 커밋

--- src/components/ResourceForm/index.jsx
+++ src/components/ResourceForm/index.jsx
@@ -1,16 +1,72 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { toast } from "react-hot-toast";
 import FileInput from "./FileInput";
 
 function ResourceForm() {
-  const [selectedFile, setSelectedFile] = useState(null);
-  const fileInputRef = useRef(null);
+  const [requiredLogoDetails, setRequiredLogoDetails] = useState({
+    name: "",
+    description: "",
+    default: null,
+    categoryId: null,
+  });
 
-  function handleFileChange(event) {
-    setSelectedFile(event.target.files[0]);
+  const [logoImagesByMode, setLogoImagesByMode] = useState({
+    darkmode: null,
+    "1.5x": null,
+    "2x": null,
+    "3x": null,
+    "4x": null,
+  });
+
+  const [previewSource, setPreviewSource] = useState(null);
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setRequiredLogoDetails(prevData => ({ ...prevData, [name]: value }));
+  }
+
+  function onDrop(acceptedFiles) {
+    const file = acceptedFiles[0];
+
+    if (file && file.type !== "image/svg+xml") {
+      toast.error("Selected file is not SVG.\nPlease choose SVG file! :)");
+
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setPreviewSource(reader.result);
+    };
+
+    setRequiredLogoDetails(prevFiles => ({ ...prevFiles, default: file }));
+  }
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  function handleFileChange(event, mode) {
+    if (event.target.files.length === 0) {
+      return;
+    }
+
+    const file = event.target.files[0];
+
+    if (file && file.type !== "image/svg+xml") {
+      toast.error("Selected file is not SVG.\nPlease choose SVG file! :)");
+
+      return;
+    }
+
+    setLogoImagesByMode(prevFiles => ({ ...prevFiles, [mode]: file }));
   }
 
   function handleSubmit(event) {
     event.preventDefault();
+    console.log(requiredLogoDetails);
+    console.log(logoImagesByMode);
     // Add submit logic here
   }
 
@@ -20,9 +76,21 @@ function ResourceForm() {
         <div className="grid grid-cols-3 h-fit">
           <div>
             <h2 className="text-lg font-bold">Brand logo</h2>
-            <div className="border border-stone-800 rounded-md w-60 h-60">
-              {" "}
+            {/* eslint-disable react/jsx-props-no-spreading */}
+            <div
+              {...getRootProps()}
+              className="border border-stone-800 rounded-md w-60 h-60"
+            >
+              <input {...getInputProps()} />
+              {previewSource && (
+                <img
+                  src={previewSource}
+                  alt="chosen"
+                  style={{ width: "100%", height: "100%" }}
+                />
+              )}
             </div>
+            {/* eslint-enable react/jsx-props-no-spreading */}
             <p className="text-xs text-stone-400">
               * Please set your brand signature logo.
             </p>
@@ -30,13 +98,19 @@ function ResourceForm() {
           <div className="col-span-2 flex flex-col">
             <h2 className="text-lg font-bold">Name</h2>
             <input
+              name="name"
               className="items-center mb-5 border border-stone-800 rounded-md h-7 bg-transparent placeholder-stone-400 placeholder:text-xs"
               placeholder="Please write the log name of your brand"
+              value={requiredLogoDetails.name}
+              onChange={handleInputChange}
             />
             <h2 className="text-lg font-bold">Description</h2>
             <textarea
+              name="description"
               className="flex-grow border border-stone-800 rounded-md bg-transparent placeholder-stone-400 placeholder:text-xs"
               placeholder="Please tell your team members what to pay attention to when using the brand logo."
+              value={requiredLogoDetails.description}
+              onChange={handleInputChange}
             />
             <p className="text-xs text-stone-400">
               * The content will be displayed on the main page.
@@ -44,13 +118,12 @@ function ResourceForm() {
           </div>
         </div>
         <div className="mt-10">
-          {["Dark mode", "1.5x", "2x", "3x", "4x"].map(mode => (
+          {Object.keys(logoImagesByMode).map(mode => (
             <FileInput
               key={mode}
-              label={mode}
-              fileInputRef={fileInputRef}
-              handleFileChange={handleFileChange}
-              selectedFile={selectedFile}
+              mode={mode}
+              handleFileChange={event => handleFileChange(event, mode)}
+              logoImageByMode={logoImagesByMode[mode]}
             />
           ))}
         </div>
