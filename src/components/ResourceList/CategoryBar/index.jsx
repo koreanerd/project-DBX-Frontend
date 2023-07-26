31fa032833244acb7f33363a664537a943f2600f
koreaNerd <pksksk0147@gmail.com>
2023-07-26 20:16:26 +0900

    feat: ResourceList 핵심기능 모듈화

---
+++ src/components/ResourceList/CategoryBar/index.jsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line react/prop-types
+function CategoryBar({ categories, activeCategory, onChangeCategory }) {
+  return (
+    <div className="grid grid-rows-5 grid-flow-col pl-6 pr-16 w-1/5 h-full">
+      <div className="row-start-2 row-span-2 border-t-2 border-stone-400">
+        <ul className="mt-6">
+          {/* eslint-disable-next-line react/prop-types */}
+          {categories.map(category => (
+            <li key={category} className="hover:bg-stone-200">
+              <button
+                type="button"
+                className={`ml-7 mb-1 text-xl text-stone-500 cursor-default ${
+                  activeCategory === category ? "font-bold text-stone-800" : ""
+                }`}
+                onClick={() => onChangeCategory(category)}
+              >
+                {category}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default CategoryBar;
---
+++ src/components/ResourceList/ControlPanel/index.jsx
@@ -0,0 +1,14 @@
+function ControlPanel() {
+  return (
+    <div className="w-1/5 h-full drop-shadow-2xl bg-stone-300">
+      <div className="flex items-center bg-stone-100 h-16 text-stone-500">
+        <div className="flex items-center ml-6">
+          <div className="w-8 h-8 rounded-md bg-green-400"> </div>
+          <p className="ml-4">dbx5173@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ControlPanel;
