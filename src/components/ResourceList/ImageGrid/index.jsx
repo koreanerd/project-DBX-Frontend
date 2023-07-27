97530539038ecc7d3de61e222ec50af6b3dc8563
koreaNerd <pksksk0147@gmail.com>
2023-07-27 15:37:01 +0900

    chore: ImageGrid 컴포넌트 key값 수정

--- src/components/ResourceList/ImageGrid/index.jsx
+++ src/components/ResourceList/ImageGrid/index.jsx
@@ -4,12 +4,15 @@ function ImageGrid({ category }) {
     <div className="w-3/5 overflow-auto h-screen">
       <div className="grid grid-cols-4 gap-4 mt-12 mr-12">
         {/* eslint-disable-next-line react/prop-types */}
-        {category.map((img, i) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <div key={i} className="bg-stone-100 rounded-xl">
-            <img src={img} alt="" />
-          </div>
-        ))}
+        {category.map(img => {
+          const objectID = img.split("/")[3].replace(/"/g, "");
+
+          return (
+            <div key={objectID} className="bg-stone-100 rounded-xl">
+              <img src={img} alt="" />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
